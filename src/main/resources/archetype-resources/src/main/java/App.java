61f79bd81b8c4b8e42538150ee6501dec06cfce4
Costel Radulescu <wizard.ro@gmail.com>
2015-09-26 15:09:25 +0300

    initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,12 @@
+package ${package};
+
+public class App {
+
+    private App() {
+    }
+
+    public static void main(final String [] args) {
+        System.out.println("Simple java application");
+    }
+}
+
---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,14 @@
+package ${package};
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
+
